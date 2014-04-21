7af8915d149512b0e84d589268b46f89ed0a3012
Ronan Murphy <ronan_murfy@hotmail.com>
2014-04-21 09:24:44 +0100

    First Commit

---
+++ LeagueHelper/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿using System.Drawing;
+namespace LeagueHelper
+{
+    partial class LeagueHelper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LeagueHelper));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LeagueHelper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 562);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "LeagueHelper";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LeagueHelper";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LeagueHelper/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LeagueHelper
+{
+    public partial class LeagueHelper : Form
+    {
+        private Timer timer;
+        private System.Media.SoundPlayer player;
+        private int oldRed;
+
+        private int recX;
+        private int recY;
+        private int recW;
+        private int recH;
+
+        private int accuracy;
+
+
+        public LeagueHelper()
+        {
+            string fileName = "Settings\\properties.txt";
+
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+
+            int interval = Convert.ToInt32(lines[1]);
+            this.recX = Convert.ToInt32(lines[4]);
+            this.recY = Convert.ToInt32(lines[5]);
+            this.recW = Convert.ToInt32(lines[6]);
+            this.recH = Convert.ToInt32(lines[7]);
+
+            this.accuracy = Convert.ToInt32(lines[10]);
+
+
+
+            this.timer = new Timer();
+            this.timer.Interval = interval;
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(OnTimerEvent);
+
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+
+            this.player = new System.Media.SoundPlayer("Sounds\\alert.wav");
+
+            this.oldRed = 0;
+
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //player.Play();
+        }
+
+        public void OnTimerEvent(object source, EventArgs e)
+        {
+            Graphics myGraphics = this.CreateGraphics();
+            Size s = new Size(recW, recH);
+            Bitmap memoryImage = new Bitmap(s.Width, s.Height, myGraphics);
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+            memoryGraphics.CopyFromScreen(recX, recY, 0, 0, s);
+
+            this.oldRed = checkData(memoryImage, this.oldRed);
+
+            pictureBox1.Image = memoryImage;
+        }
+
+        private int checkData(Bitmap memoryImage, int oldRed)
+        {
+            int championMin = this.accuracy;
+
+            int newRed = getRedContent(memoryImage);
+
+            if ((newRed - oldRed) > championMin)
+            {
+                alert();
+            }
+            return newRed;
+        }
+
+        private void alert()
+        {
+            player.Play();
+        }
+
+        private int getRedContent(Bitmap image)
+        {
+            Color pixel;
+            int redCount = 0;
+            int redMin = 234;
+            int greMax = 20;
+            int bluMax = 20;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    pixel = image.GetPixel(i, j);
+                    if ((pixel.R > redMin) &&
+                        (pixel.G < greMax) &&
+                        (pixel.B < bluMax))
+                    {
+                        redCount++;
+                    }
+                }
+            }
+            return redCount;
+        }
+    }
+}
